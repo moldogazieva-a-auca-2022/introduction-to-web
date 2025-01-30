5858f2b8838c935f53b875524a915542a62130c9
Ailin Moldogazieva <moldogazieva_a@auca.kg>
2025-01-30 11:04:50 +0600

    Added templates

--- lab04/flask_app/app.py
+++ lab04/flask_app/app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello World!'
-
 @app.route('/hello/<name>')
-def hello(name):
-    return f"Hello {name}!"
+def hello_name(name):
+    return render_template("hello.html", name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
