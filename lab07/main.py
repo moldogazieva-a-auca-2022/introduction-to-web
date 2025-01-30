c65bfc270c025cddb7ee69839d843c898115134d
Ailin Moldogazieva <moldogazieva_a@auca.kg>
2025-01-30 11:30:38 +0600

    Finished Lab 7

---
+++ lab07/main.py
@@ -0,0 +1,19 @@
+print("Using for loop:")
+for i in range(1, 11):
+    print(i)
+
+print("\nUsing while loop:")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+print("\nIterating over 'Python' with a for loop:")
+for char in "Python":
+    print(char)
+
+print("\nUsing while loop to print even numbers from 2 to 10:")
+num = 2
+while num <= 10:
+    print(num)
+    num += 2
